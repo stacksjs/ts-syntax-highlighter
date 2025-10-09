04bab06b66775f4460da74da3571adf00e79c61c
Chris <chrisbreuer93@gmail.com>
2025-10-08 18:07:31 -0700

    chore: wip

--- packages/ts-syntax-highlighter/src/types.ts
+++ packages/ts-syntax-highlighter/src/types.ts
@@ -2,4 +2,4 @@ export interface SyntaxHighlighterConfig {
   verbose: boolean
 }
 
-type SyntaxHighlighterOptions = Partial<SyntaxHighlighterConfig>
+export type SyntaxHighlighterOptions = Partial<SyntaxHighlighterConfig>
